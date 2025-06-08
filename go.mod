9bf638f8707d099f3e08db92a92cbb8fb658ef6c
Test User <test@example.com>
2025-06-08 15:41:52 +0800

    feat(swagger): 添加Swagger文档支持并更新API路由
    - 引入Swagger文档支持，新增处理Swagger文档的HTTP处理函数。
    - 动态修改Swagger文档中的路径，确保所有路径前缀为/api。
    - 注册Swagger UI处理器，提供Swagger文档的可视化界面。
    - 更新API路由配置，确保与新Swagger支持兼容。

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gorilla/mux v1.8.1
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3
 	github.com/spf13/pflag v1.0.6
+	github.com/swaggo/http-swagger v1.3.4
 	go.uber.org/zap v1.15.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb
 	google.golang.org/grpc v1.72.2
@@ -15,19 +16,30 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/spec v0.20.6 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	sigs.k8s.io/json v0.0.0-20241010143419-9aa6b5e7a4b3 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.6.0 // indirect
 	sigs.k8s.io/yaml v1.4.0 // indirect
--- internal/helloworld/installer.go
+++ internal/helloworld/installer.go
@@ -4,15 +4,25 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/costa92/go-protoc/internal/helloworld/service"
 	v1 "github.com/costa92/go-protoc/pkg/api/helloworld/v1"
 	v2 "github.com/costa92/go-protoc/pkg/api/helloworld/v2"
-	"github.com/costa92/go-protoc/pkg/response" // Import the response package
+	"github.com/costa92/go-protoc/pkg/response"
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	apiPrefix    = "/api"
+	swaggerPath  = "/swagger"
+	swaggerDoc   = "/swagger/doc.yaml"
+	swaggerIndex = "/swagger/index.html"
 )
 
 // APIGroupInstaller 实现了 server.Installer 接口
@@ -34,9 +44,53 @@ func NewInstaller(logger *zap.Logger) *APIGroupInstaller {
 	}
 }
 
+// handleSwaggerDoc serves the swagger.yaml file, dynamically adding the "/api" prefix to all paths.
+func (i *APIGroupInstaller) handleSwaggerDoc(w http.ResponseWriter, r *http.Request) {
+	fileBytes, err := os.ReadFile("api/swagger/swagger.yaml")
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("swagger.yaml not found"))
+		i.logger.Error("failed to read swagger.yaml", zap.Error(err))
+		return
+	}
+
+	var data map[string]interface{}
+	if err := yaml.Unmarshal(fileBytes, &data); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to parse swagger.yaml"))
+		i.logger.Error("failed to unmarshal swagger.yaml", zap.Error(err))
+		return
+	}
+
+	if paths, ok := data["paths"].(map[string]interface{}); ok {
+		newPaths := make(map[string]interface{})
+		for key, value := range paths {
+			newPaths[apiPrefix+key] = value
+		}
+		data["paths"] = newPaths
+	}
+
+	modifiedBytes, err := yaml.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("failed to generate modified swagger.yaml"))
+		i.logger.Error("failed to marshal modified swagger.yaml", zap.Error(err))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/yaml")
+	w.Write(modifiedBytes)
+}
+
 // Install 实现 server.Installer 接口
 func (i *APIGroupInstaller) Install(router *mux.Router) error {
-	// 创建 grpc-gateway 的 ServeMux
+	router.HandleFunc(swaggerDoc, i.handleSwaggerDoc)
+
+	router.PathPrefix(swaggerPath + "/").Handler(httpSwagger.Handler(
+		httpSwagger.URL(swaggerDoc),
+	))
+	i.logger.Info("registered swagger ui handler", zap.String("path", swaggerIndex))
+
 	gwmux := runtime.NewServeMux(
 		runtime.WithErrorHandler(response.CustomHTTPErrorHandler),
 		runtime.WithMarshalerOption(runtime.MIMEWildcard, &response.CustomMarshaler{
@@ -44,26 +98,22 @@ func (i *APIGroupInstaller) Install(router *mux.Router) error {
 		}),
 	)
 
-	// 注册 v1 的 HTTP 路由
 	err := v1.RegisterGreeterHandlerServer(context.Background(), gwmux, i.v1Service)
 	if err != nil {
 		return fmt.Errorf("failed to register v1 handler: %w", err)
 	}
 
-	// 注册 v2 的 HTTP 路由
 	err = v2.RegisterGreeterHandlerServer(context.Background(), gwmux, i.v2Service)
 	if err != nil {
 		return fmt.Errorf("failed to register v2 handler: %w", err)
 	}
 
-	// 将 gateway mux 挂载到路由器，使用 PathPrefix 确保所有请求都带有 /api 前缀
-	router.PathPrefix("/api/").Handler(http.StripPrefix("/api", gwmux))
+	router.PathPrefix(apiPrefix + "/").Handler(http.StripPrefix(apiPrefix, gwmux))
 	return nil
 }
 
 // RegisterGRPC 实现 server.Installer 接口
 func (i *APIGroupInstaller) RegisterGRPC(srv *grpc.Server) error {
-	// 注册 v1 和 v2 的 gRPC 服务
 	v1.RegisterGreeterServer(srv, i.v1Service)
 	v2.RegisterGreeterServer(srv, i.v2Service)
 	return nil
