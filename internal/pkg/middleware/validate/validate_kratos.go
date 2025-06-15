57eb2ff97bb4cb659e2f1482c0fdbf7e4faa4224
Test User <test@example.com>
2025-06-15 20:44:44 +0800

    重构代码代码

---
+++ internal/pkg/middleware/validate/validate_kratos.go
@@ -0,0 +1,33 @@
+package validate
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/middleware"
+
+	"github.com/costa92/go-protoc/v2/pkg/api/errno"
+)
+
+// RequestValidator 定义了用于自定义验证的接口.
+type RequestValidator interface {
+	Validate(ctx context.Context, rq any) error
+}
+
+// Validator is a validator middleware.
+func Validator(validator RequestValidator) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, rq any) (reply any, err error) {
+			// Custom validation, specific to the API interface
+			if err := validator.Validate(ctx, rq); err != nil {
+				if se := new(errors.Error); errors.As(err, &se) {
+					return nil, se
+				}
+
+				return nil, errno.ErrorInvalidParameter(err.Error()).WithCause(err)
+			}
+
+			return handler(ctx, rq)
+		}
+	}
+}
---
+++ pkg/errors/doc.go
@@ -0,0 +1,2 @@
+// Package errors makes a set of error message handlers available for use by cloud miner.
+package errors
