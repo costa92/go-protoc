83d6206fbe4c28d1b388fd5c3308a0b2270a12dd
Test User <test@example.com>
2025-06-22 23:53:21 +0800

    fix(validation/user): 添加用户创建请求的默认值设置
    在 ValidateCreateUserRequest 方法中增加了对 Name 和 Email 字段的默认值设置，并添加了日志记录。

--- internal/apiserver/pkg/validation/user.go
+++ internal/apiserver/pkg/validation/user.go
@@ -2,6 +2,7 @@ package validation
 
 import (
 	"context"
+	"log"
 
 	"github.com/costa92/go-protoc/v2/internal/apiserver/pkg/locales"
 	v1 "github.com/costa92/go-protoc/v2/pkg/api/apiserver/v1"
@@ -16,6 +17,17 @@ func (v *Validator) ValidateUserRules() genericvalidation.Rules {
 
 // ValidateCreateUserRequest validates the fields of a CreateUserRequest.
 func (v *Validator) ValidateCreateUserRequest(ctx context.Context, rq *v1.CreateUserRequest) error {
+	log.Println("ValidateCreateUserRequest")
+
+	// 设置默认值
+	if rq.Name == "" {
+		rq.Name = "default_user"
+	}
+
+	if rq.Email == "" {
+		rq.Email = "default@example.com"
+	}
+
+	// 检查用户是否已存在
 	return i18n.FromContext(ctx).E(locales.UserAlreadyExists)
-	// return nil
 }
